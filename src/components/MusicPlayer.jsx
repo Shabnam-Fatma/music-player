1f27ee4a08e14abd6b75b80cb1f16461990294d4
Shabnam-Fatma <shabnamfatma.tech@gmail.com>
2026-06-06 23:25:27 +0530

    Adding played tack

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { useMusic } from "../hooks/useMusic";
 
 const MusicPlayer = () => {
-  const { currentTrack } = useMusic();
+  const { currentTrack, formatTime, currentTime } = useMusic();
   return (
     <div className="music-player">
       <audio />
       <div className="track-info">
         <h3 className="track-title">{currentTrack.title}</h3>
         <p className="track-artist">{currentTrack.artist}</p>
+      </div>
+      <div className="progress-container">
+        <span className="time">{formatTime(currentTime)}</span>
       </div>
     </div>
   );
--- src/hooks/useMusic.js
+++ src/hooks/useMusic.js
@@ -63,11 +63,29 @@ export const useMusic = () => {
   const [allSongs, setAllSongs] = useState(songs);
   const [currentTrack, setCurrentTrack] = useState(songs[0]);
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [currentTime, setCurrentTtime] = useState(0);
 
   const handlePlaySong = (song, index) => {
     setCurrentTrack(song);
     setCurrentTrackIndex(index);
   };
 
-  return { allSongs, handlePlaySong, currentTrackIndex, currentTrack };
+  const formatTime = (time) => {
+    if (isNaN(time) || time === undefined) return "0:00";
+
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`
+  };
+
+  return {
+    allSongs,
+    handlePlaySong,
+    currentTrackIndex,
+    currentTrack,
+    currentTime,
+    setCurrentTtime,
+    formatTime
+  };
 };
