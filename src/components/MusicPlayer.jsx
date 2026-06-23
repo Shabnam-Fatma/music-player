8b92377997f21c7407d7163d1d06f00532abe176
Shabnam-Fatma <shabnamfatma.tech@gmail.com>
2026-06-23 22:31:13 +0530

    added range input

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useMusic } from "../hooks/useMusic";
 
 const MusicPlayer = () => {
   const { currentTrack, formatTime, currentTime, duration } = useMusic();
+  const audioRef = useRef(null)
   return (
     <div className="music-player">
-      <audio />
+      <audio ref={audioRef} preload="metadata" crossOrigin="anonymous" />
       <div className="track-info">
         <h3 className="track-title">{currentTrack.title}</h3>
         <p className="track-artist">{currentTrack.artist}</p>
       </div>
       <div className="progress-container">
         <span className="time">{formatTime(currentTime)}</span>
+        <input
+          type="range"
+          min="0"
+          max={duration || 0}
+          step="0.1"
+          value={currentTime || 0}
+          className="progress-bar"
+          // style={{}}
+        />
         <span className="time">{formatTime(duration)}</span>
       </div>
     </div>
